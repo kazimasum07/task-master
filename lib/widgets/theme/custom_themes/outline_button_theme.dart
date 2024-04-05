b99b24cbdafa0645ef13c2f4274745e18053c5b5
Kazi Masum <masum15-12549@diu.edu.bd>
2024-04-05 15:30:59 +0600

    custom outline button theme for light and dark mode

---
+++ lib/widgets/theme/custom_themes/outline_button_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:task_master/widgets/constants/colors.dart';
+import 'package:task_master/widgets/constants/text_styles.dart';
+
+
+class TMOutlineButtonTheme{
+  TMOutlineButtonTheme._();
+
+  static final lightOutlineButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: TMCustomColors.blackColor,
+      side: const BorderSide(color: TMCustomColors.blueColor),
+      textStyle: TMCustomTextStyle.outlineButtonTextStyleLight,
+      padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(28))
+    )
+  );
+  static final darkOutlineButtonTheme = OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+          elevation: 0,
+          foregroundColor: TMCustomColors.whiteColor,
+          side: const BorderSide(color: TMCustomColors.blueAccentColor),
+          textStyle: TMCustomTextStyle.outlineButtonTextStyleLight,
+          padding: const EdgeInsets.symmetric(vertical: 16,horizontal: 20),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(28))
+      )
+  );
+}
