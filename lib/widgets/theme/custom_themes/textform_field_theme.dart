7d2174e797d75ea6e5900f58edfde2dc4038ed10
Kazi Masum <masum15-12549@diu.edu.bd>
2024-04-05 15:31:34 +0600

    custom TextFormField theme for light and dark mode

---
+++ lib/widgets/theme/custom_themes/textform_field_theme.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:task_master/widgets/constants/colors.dart';
+
+class TMTextFormFieldTheme{
+  TMTextFormFieldTheme._();
+
+  static InputDecorationTheme lightInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: TMCustomColors.greyColor,
+    suffixIconColor: TMCustomColors.greyColor,
+    labelStyle: const TextStyle().copyWith(fontSize: 14,color: TMCustomColors.blackColor),
+    hintStyle: const TextStyle().copyWith(fontSize: 14,color: TMCustomColors.blackColor),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+    floatingLabelStyle: const TextStyle().copyWith(color: TMCustomColors.blackColor.withOpacity(0.8)),
+    border: const OutlineInputBorder().copyWith(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(width: 1,color: TMCustomColors.greyColor)
+    ),
+    enabledBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.greyColor)
+    ),
+    focusedBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.black12Color)
+    ),
+    errorBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.redColor)
+    ),
+    focusedErrorBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.orangeColor)
+    ),
+  );
+  static InputDecorationTheme darkInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: TMCustomColors.greyColor,
+    suffixIconColor: TMCustomColors.greyColor,
+    labelStyle: const TextStyle().copyWith(fontSize: 14,color: TMCustomColors.whiteColor),
+    hintStyle: const TextStyle().copyWith(fontSize: 14,color: TMCustomColors.whiteColor),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+    floatingLabelStyle: const TextStyle().copyWith(color: TMCustomColors.blackColor.withOpacity(0.8)),
+    border: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.greyColor)
+    ),
+    enabledBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.greyColor)
+    ),
+    focusedBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.whiteColor)
+    ),
+    errorBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.redColor)
+    ),
+    focusedErrorBorder: const OutlineInputBorder().copyWith(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(width: 1,color: TMCustomColors.orangeColor)
+    ),
+  );
+}
