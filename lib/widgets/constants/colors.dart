8a66ea840916a957eef4ee2634becbfda2064626
Kazi Masum <masum15-12549@diu.edu.bd>
2024-04-05 15:26:38 +0600

    Setting custom and constant Colors

---
+++ lib/widgets/constants/colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TMCustomColors{
+  TMCustomColors._();
+  static const Color primaryColor =Colors.blue;
+  static const Color blueColor =Colors.blue;
+  static const Color blueAccentColor =Colors.blueAccent;
+  static const Color blackColor =Colors.black;
+  static const Color black12Color =Colors.black12;
+  static const Color greyColor =Colors.grey;
+  static const Color redColor =Colors.red;
+  static const Color orangeColor =Colors.orange;
+  static const Color whiteColor =Colors.white;
+  static const Color transparentColor =Colors.transparent;
+
+  ///Elevated Button
+  static const Color elevatedButtonSideColor =Colors.blue;
+  static const Color elevatedButtonTextColor =Colors.white;
+  static const Color elevatedButtonForegroundColor =Colors.white;
+  static const Color elevatedButtonBackgroundColor =Colors.blue;
+  static const Color elevatedButtonDisabledForegroundColor =Colors.grey;
+  static const Color elevatedButtonDisabledBackgroundColor =Colors.grey;
+
+
+  static Color appBarBackgroundColor = Colors.transparent;
+  static Color appBarSurfaceTintColor = Colors.transparent;
+  static Color appBarTextColorDark = Colors.white;
+  static Color appBarTextColorLight = Colors.black;
+
+}
