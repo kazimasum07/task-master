47d824338926af39a872380f9f5a312cea39b458
Kazi Masum <masum15-12549@diu.edu.bd>
2024-04-05 15:28:55 +0600

    custom text theme for light and dark mode

---
+++ lib/widgets/theme/custom_themes/text_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class TMTextTheme{
+  TMTextTheme._();
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: const TextStyle().copyWith(fontSize: 32,fontWeight: FontWeight.bold,color: Colors.black),
+    headlineMedium: const TextStyle().copyWith(fontSize: 24,fontWeight: FontWeight.w600,color: Colors.black),
+  );
+  static TextTheme darkTextTheme = TextTheme(
+    headlineLarge: const TextStyle().copyWith(fontSize: 32,fontWeight: FontWeight.bold,color: Colors.white),
+    headlineMedium: const TextStyle().copyWith(fontSize: 24,fontWeight: FontWeight.w600,color: Colors.white),
+  );
+
+}
